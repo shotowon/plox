4c04c99ce759595f861d48f2bb22f74f86cce72e
shotowon <sanzhar.maratov@proton.me>
2025-01-30 02:58:31 +0500

    scanner: advance, peek, match

--- plox/scanner.py
+++ plox/scanner.py
@@ -1,4 +1,4 @@
-from typing import List, Iterator, Any
+from typing import Iterator, Any
 
 from plox.tokens import TokenType, Token
 
@@ -13,8 +13,9 @@ class Scanner:
 
     def scan_token(self) -> Token:
         token: Token = self.__new_token()
-        char: str | None = self.__advance()
-        while char is not None and char.isspace():
+        char: str = self.__advance()
+
+        while char != "\0" and char.isspace():
             if char == "\n":
                 self.__line += 1
             self.__start = self.__current
@@ -43,18 +44,32 @@ class Scanner:
                 token = self.__new_token(TokenType.SLASH)
             case ";":
                 token = self.__new_token(TokenType.SEMICOLON)
-            case None:
+            case "\0":
                 token = self.__new_token(TokenType.EOF)
 
         self.__start = self.__current
         self.__current_token = token
         return token
 
-    def __advance(self) -> str | None:
+    def __advance(self) -> str:
         if self.__is_source_end():
-            return None
+            return "\0"
         self.__current += 1
         return self.__source[self.__current - 1]
+
+
+    def __match(self, next: str) -> bool:
+        if self.__peek() != next and next != "\0":
+            return False
+        self.__current += 1
+        return True
+
+    def __peek(self, offset: int = 0) -> str:
+        return (
+            "\0"
+            if self.__current + offset >= len(self.__source)
+            else self.__source[self.__current + offset]
+        )
 
     def __is_source_end(self) -> bool:
         return self.__current >= len(self.__source)
@@ -71,7 +86,6 @@ class Scanner:
             lexeme=lexeme,
             literal=literal,
             line=self.__line,
-        )
             meta=meta,
         )
 
